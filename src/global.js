db830a45f267923414fc00cf166605f40e99c455
Emil Hernqvist <e.g.hernqvist@gmail.com>
2018-10-01 13:26:54 +0200

    removed self

--- src/global.js
+++ src/global.js
@@ -1,3 +1,3 @@
-export const global = (typeof self === 'object' && self.self === self && self) || (typeof global === 'object' && global.global === global && global) || this;
+export const global = (typeof global === 'object' && global.global === global && global) || this;
 
 export const document = global ? global.document : false || {};
